cb2a91f1ba4702cf4aa8f94139f0990e8db0462d
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2024-03-16 16:32:04 +0700

    chore(): set up main.ts config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { satisfies } from 'semver';
+import { engines } from '../package.json';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
+  const nodeVersion = engines.node;
+  if (!satisfies(process.version, nodeVersion)) {
+    console.log(
+      `Required node version ${nodeVersion} not satisfied with current version ${process.version}.`,
+    );
+    process.exit();
+  }
+
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+    credentials: false,
+  });
+
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
