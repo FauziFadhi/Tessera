5a7a7c10c8e8431774e5db424e710016a3dc33d0
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2024-03-18 23:30:14 +0700

    chore(): change typeorm entities and migration with glob path

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -1,32 +1,24 @@
-import { City } from '@modules/city/entities/city.entity';
-import { Event } from '@modules/event/entities/event.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import { CreateCityTable1710598371355 } from '@modules/city/db/migrations/1710598371355-create_city_table';
-import { CreateEventTable1710656724153 } from '@modules/event/db/migrations/1710656724153-create_event_table';
-import { AddUniqueNameEventTable1710776435330 } from '@modules/event/db/migrations/1710776435330-add_unique_name_event_table';
+import { join } from 'path';
 
 export const options: DataSourceOptions = {
   type: 'sqlite',
   database: 'tessera.sqlite',
-  entities: [City, Event],
   migrationsRun: true,
+  entities: [join(__dirname, '..', 'modules', '**', 'entities', '*{.ts,.js}')],
   migrations: [
-    CreateCityTable1710598371355,
-    CreateEventTable1710656724153,
-    AddUniqueNameEventTable1710776435330,
+    join(__dirname, '..', 'modules', '**', 'db', 'migrations', '*{.ts,.js}'),
   ],
 };
 
 export const optionsTest: DataSourceOptions = {
   type: 'sqlite',
   database: 'tessera-test.sqlite',
-  entities: [City, Event],
   migrationsRun: true,
   dropSchema: true,
+  entities: [join(__dirname, '..', 'modules', '**', 'entities', '*{.ts,.js}')],
   migrations: [
-    CreateCityTable1710598371355,
-    CreateEventTable1710656724153,
-    AddUniqueNameEventTable1710776435330,
+    join(__dirname, '..', 'modules', '**', 'db', 'migrations', '*{.ts,.js}'),
   ],
 };
 const datasource = new DataSource(options);
